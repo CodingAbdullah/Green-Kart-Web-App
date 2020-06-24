316f6783289a88397613624ef9f1d941ced92cbe
Abdul <abdullahmd326@gmail.com>
2020-06-24 16:05:04 -0400

    Cleaning up

--- back-end/router/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const userController = require("../controller/UserController");
-const router = express.Router();
-
-router.get("/", userController.homePageFunction);
-router.post("/form", userController.formValidation);
-
-module.exports = router;
